69e387d60a5e8f28028b09b0936bd863bec2776f
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-06-28 19:22:07 +0530

    added app.js

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,11 @@
+// variables and constants
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3/2 -1;
+let calculationDescription = 
+'(' + defaultResult + ' + 10) * 3/2 -1';
+
+// alert(currentResult);
+
+outputResult(currentResult, calculationDescription);
